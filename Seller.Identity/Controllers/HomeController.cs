620d7f7f2a0162a36739ab8a3576b18b31e658f9
Remberto Martinez <remberto.martinez@elisa.fi>
2018-03-19 12:22:25 +0200

    updated sample for ws auth

---
+++ Seller.Identity/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.WsFederation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Seller.Identity.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return Ok("Welcome Home");
+        }
+
+        [Route("login")]
+        public IActionResult SignIn()
+        {
+            if (User == null || !User.Identities.Any(identity => identity.IsAuthenticated))
+            {
+                var redirectUrl = Url.Action(nameof(Index), "Home");
+                return Challenge(
+                    new AuthenticationProperties { RedirectUri = redirectUrl },
+                    // OpenIdConnectDefaults.AuthenticationScheme
+                    WsFederationDefaults.AuthenticationScheme);
+            }           
+
+
+            return RedirectToAction(nameof(AppNext)); ;
+        }
+
+        [Authorize]
+        [Route("app")]
+        public IActionResult AppNext()
+        {
+            // check are we auth
+            if (!User.Identity.IsAuthenticated && !User.Identity.AuthenticationType.Equals("AuthenticationTypes.Federation"))
+            {
+                return Forbid();
+            }
+    
+
+            try
+            {            
+                if (!User.Identities.Any(identity => identity.HasClaim("http://schemas.microsoft.com/identity/claims/identityprovider", "live.com") || identity.HasClaim("rem", "true")))
+                {
+                    Response.WriteAsync($"Not authorized without claims for user {User.Identity.Name}");
+                    return Forbid();
+                }
+
+                var currentClaimsIdentity = User.Identities.FirstOrDefault(identity =>
+                    identity.HasClaim("http://schemas.microsoft.com/identity/claims/identityprovider", "live.com"));
+                var claimRem = new Claim(ClaimTypes.Name, "rem.org");
+                currentClaimsIdentity?.AddClaim(claimRem);
+                var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(currentClaimsIdentity?.Claims, CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role));
+
+                SignIn(claimsPrincipal, CookieAuthenticationDefaults.AuthenticationScheme);
+                var claimLive = currentClaimsIdentity?.Claims.FirstOrDefault(id => id.Type.Equals("http://schemas.microsoft.com/identity/claims/displayname"));
+                return Ok($"Welcome to Portal {claimLive?.Value}");
+            }
+            catch
+            {
+                return Unauthorized();
+            }
+
+           
+        }
+
+        [HttpGet]
+        public IActionResult AccessDenied()
+        {
+            Response.WriteAsync($"Access Denied for user {User.Identity.Name}");
+            return Unauthorized();
+        }
+
+
+
+
+    }
+}
---
+++ Seller.Identity/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.WsFederation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Seller.Identity
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(sharedOptions =>
+                {
+                    sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    sharedOptions.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    sharedOptions.DefaultChallengeScheme = WsFederationDefaults.AuthenticationScheme;
+                })
+                .AddWsFederation(options =>
+                {
+                    options.Wtrealm = Configuration["wsfed:realm"];
+                    options.MetadataAddress = Configuration["wsfed:metadata"];
+                })
+                .AddCookie();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+
+            try
+            {
+                if (env.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                }
+                else
+                {
+                    app.UseExceptionHandler();
+                }
+
+                app.UseAuthentication();
+                app.UseMvc(routes =>
+                {
+                    routes.MapRoute(
+                        "default",
+                        "{controller=Home}/{action=Index}/{id?}");
+                });
+
+            }
+            catch (Exception e)
+            {
+                app.Run(async context =>
+                {
+                    await context.Response.WriteAsync(HtmlEncoder.Default.Encode($"user {context.User.Identity.Name} , {e }, {e.Message}"));
+              
+                    context.Response.StatusCode = 500;
+                    
+                });
+            }
+        }
+    }
+}
